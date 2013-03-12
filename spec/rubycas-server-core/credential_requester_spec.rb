a05959b47c73673d7cd3b403b93f91c1deea2e75
Tyler Pickett <t.pickett66@gmail.com>
2013-03-12 15:43:51 -0500

    begin implementing credential requester
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/rubycas-server-core/credential_requester_spec.rb
+++ spec/rubycas-server-core/credential_requester_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe RubyCAS::Server::Core::CredentialRequester do
+  let(:controller) { double("Controller") }
+  subject { described_class.new(controller) }
 
-  # new login
-    # generate login ticket
-    # persist login ticket
-    # send user_not_logged_in(ticket_string)
+  describe 'new login' do
+    let(:params) { {} }
+    let(:cookies) { {} }
+
+    before do
+      RubyCAS::Server::Core::Tickets.stub(:generate_login_ticket) {
+        OpenStruct.new({ticket: 'LT-123ABC'})
+      }
+      controller.should_receive(:user_not_logged_in).with("LT-123ABC")
+    end
+
+    it 'must satisfy our expectations' do
+      subject.process!(params, cookies)
+    end
+  end
 
   # already logged in
     # if session is good
