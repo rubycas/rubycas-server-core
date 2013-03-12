6df492c84e756abef8e2dbd146ad4f530e4d936e
Tyler Pickett <t.pickett66@gmail.com>
2013-03-12 15:43:51 -0500

    begin implementing credential requester
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/credential_requester.rb
+++ lib/rubycas-server-core/credential_requester.rb
@@ -1,5 +1,14 @@
 module RubyCAS::Server::Core
   class CredentialRequester
+    attr_reader :listener
 
+    def initialize(listener)
+      @listener = listener
+    end
+
+    def process!(params = {}, cookies = {})
+      ticket = Tickets.generate_login_ticket
+      listener.user_not_logged_in(ticket.ticket)
+    end
   end
 end
