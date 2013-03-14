cdff40282f70f11190c2b0b3ac8390d5f94a0441
Tyler Pickett <t.pickett66@gmail.com>
2013-03-14 13:09:16 -0500

    add todo notes for functions used in credential requester
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/tickets.rb
+++ lib/rubycas-server-core/tickets.rb
@@ -1,5 +1,22 @@
 module RubyCAS::Server::Core
   module Tickets
+    # TODO t.pickett66
+    #
+    # This is basically going to take most of the functions from the original
+    # CAS module
+    #
+    # def generate_login_ticket; end
+    # def login_ticket_valid?(lt); end
+    #
+    # def generate_ticket_granting_ticket(username, extra_attributes = {}); end
+    # def ticket_granting_ticket_valid?(tgt); end
+    # 
+    # def generate_service_ticket(service, tgt); end
+    # def service_ticket_valid?(st); end
+    #
+    # def proxy_ticket_valid?(pt); end
+    # 
+    # def generate_proxy_granting_ticket(pt)
 
   end
 end
--- lib/rubycas-server-core/util.rb
+++ lib/rubycas-server-core/util.rb
@@ -1,5 +1,9 @@
 module RubyCAS::Server::Core
   module Util
-
+    # TODO t.pickett66
+    #
+    # def build_ticketed_url(url, ticket); end
+    #
+    # def clean_service_url(url); end
   end
 end
