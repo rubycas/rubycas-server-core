d619bd7945a3bcfd6ddbbe7ee1650d14f551d2e4
Tyler Pickett <t.pickett66@gmail.com>
2013-03-14 15:09:36 -0500

    implement service url cleaner
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/util.rb
+++ lib/rubycas-server-core/util.rb
@@ -1,9 +1,21 @@
+require 'uri'
 module RubyCAS::Server::Core
   module Util
+    extend self
     # TODO t.pickett66
     #
     # def build_ticketed_url(url, ticket); end
-    #
-    # def clean_service_url(url); end
+
+    PARAM_REGEX = /(?:service|gateway|renew|ticket)=[^&]+/
+    def clean_service_url(dirty_url)
+      parsed = URI.parse(dirty_url)
+      query = parsed.query.to_s # we really only care about the query portion of the url
+      query.gsub!(PARAM_REGEX, '')
+      query.gsub!(/[\/\?&]$/, '')
+      query.gsub!(/&&/, '&')
+      query.gsub!(/^&/, '')
+      parsed.query = query.blank? ? nil : query
+      parsed.to_s
+    end
   end
 end
