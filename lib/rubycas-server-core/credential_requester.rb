a084d128c9e9d3ca4ee62eac259cb3d02e1664b5
Tyler Pickett <t.pickett66@gmail.com>
2013-03-14 15:39:37 -0500

    remove some mocked calls
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>
    Conflicts:
    	lib/rubycas-server-core/credential_requester.rb
    	spec/rubycas-server-core/credential_requester_spec.rb

--- lib/rubycas-server-core/credential_requester.rb
+++ lib/rubycas-server-core/credential_requester.rb
@@ -7,8 +7,16 @@ module RubyCAS::Server::Core
     end
 
     def process!(params = {}, cookies = {})
-      ticket = Tickets.generate_login_ticket
-      listener.user_not_logged_in(ticket.ticket)
+      tgt = cookies['tgt']
+      service = Util.clean_service_url(params['service'])
+      if tgt && Tickets.ticket_granting_ticket_valid?(tgt)
+        st = Tickets.generate_service_ticket(service, tgt)
+        target = Util.build_ticketed_url(service, st.ticket)
+        listener.user_logged_in(target)
+      else
+        ticket = Tickets.generate_login_ticket
+        listener.user_not_logged_in(ticket.ticket)
+      end
     end
   end
 end
--- spec/rubycas-server-core/credential_requester_spec.rb
+++ spec/rubycas-server-core/credential_requester_spec.rb
@@ -20,14 +20,37 @@ describe RubyCAS::Server::Core::CredentialRequester do
     end
   end
 
-  # already logged in
-    # if session is good
-      # update last_used on session
-      # send user_logged_in(target_service)
-    # if session is invalid
+  describe 'already logged in' do
+    describe 'with a valid session' do
+      let(:service) { 'https://service.test.com' }
+      let(:st) { 'ST-1234DCBA' }
+      let(:params) { {
+        'service' => service
+      } }
+      let(:cookies) { {
+        'tgt' => "TGT-4321ABCD"
+      } }
+
+      before do
+        RubyCAS::Server::Core::Tickets.should_receive(:ticket_granting_ticket_valid?).with('TGT-4321ABCD').and_return(true)
+        RubyCAS::Server::Core::Tickets.stub(:generate_service_ticket).with(service, 'TGT-4321ABCD') {
+          OpenStruct.new({ticket: st})
+        }
+
+        controller.should_receive(:user_logged_in).with("#{service}?ticket=#{st}")
+      end
+
+      it 'must satisfy our expectations' do
+        subject.process!(params, cookies)
+      end
+    end
+
+    describe 'with an expired session' do
       # ensure session is destroyed
       # generate/persist new login ticket
       # send user_not_logged_in(ticket_string)
+    end
+  end
 
   # gateway param
   # forced renew param
