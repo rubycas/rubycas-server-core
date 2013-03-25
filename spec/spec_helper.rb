1ac6d1e5f360ebd7c75c73a1245e6e0c93736037
Tyler Pickett <t.pickett66@gmail.com>
2013-03-25 15:04:31 -0500

    add Work In Progress filter to rspec filters
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,5 +12,6 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.filter_run_excluding wip: true
   config.order = 'random'
 end
