c9bdb685a883cf288e3d1b6c9d1245c7d9ee1061
Tyler Pickett <t.pickett66@gmail.com>
2013-04-29 15:41:26 -0500

    add registration ability for persistence adapters
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/persistence.rb
+++ lib/rubycas-server-core/persistence.rb
@@ -1,11 +1,21 @@
-module RubyCAS
-  module Server
-    module Core
-      module Persistence
-        def self.load_tgt(tgt_string)
-          raise NotImplementedError.new('This should be overridden by your adapter of choice')
-        end
-      end
+module RubyCAS::Server::Core
+  module Persistence
+    class AdapterNotRegisteredError < StandardError; end
+
+    @adapters ||= HashWithIndifferentAccess.new
+
+    def self.register_adapter(adapter_name, handler)
+      @adapters[adapter_name] = handler
+    end
+
+    def self.adapter_named(name)
+      @adapters.fetch(name) {
+        raise AdapterNotRegisteredError.new("No persistence adapter named #{name} has been registered, avaliable adapters are: #{@adapters.keys.join(', ')}")
+      }
+    end
+
+    def self.load_ticket_granting_ticket(tgt_string)
+      raise NotImplementedError.new('This should be overridden by your adapter of choice')
     end
   end
 end
--- spec/rubycas-server-core/persistence_spec.rb
+++ spec/rubycas-server-core/persistence_spec.rb
@@ -1,11 +1,38 @@
 require 'spec_helper'
 
 module RubyCAS::Server::Core
-  describe RubyCAS::Server::Core::Persistence do
-    describe '.load_tgt(tgt_string)' do
+  describe Persistence do
+    describe '.adapter_named' do
+      let(:name) { :awesome_adapter }
+      let(:klass) { Class.new }
+
+      it 'must raise an error when an un-registered name is supplied' do
+        expect{ Persistence.adapter_named(:garbage) }.to raise_error Persistence::AdapterNotRegisteredError
+      end
+
+      it 'must return the class registered to that name' do
+        Persistence.register_adapter(name, klass)
+        Persistence.adapter_named(name.to_s).should eq klass
+      end
+    end
+
+    describe '.register_adapter(adapter_name, handler)' do
+      let(:klass) { Class.new }
+      let(:name) { :super_awesome }
+
+      before do
+        Persistence.register_adapter(name, klass)
+      end
+
+      it 'must allow us to look up the adapter class by giving the name' do
+        Persistence.adapter_named(name).should eq klass
+      end
+    end
+
+    describe '.load_ticket_granting_ticket(tgt_string)' do
       it 'must raise an error since it will get replaced by our various implementations' do
         expect{
-          Persistence.load_tgt('TGT-ABCD')
+          Persistence.load_ticket_granting_ticket('TGT-ABCD')
         }.to raise_error NotImplementedError
       end
     end
