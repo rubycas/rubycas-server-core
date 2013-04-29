71161a9fc9f606ec5bdeed7ee50f4d601a0977ed
Tyler Pickett <t.pickett66@gmail.com>
2013-04-29 14:53:43 -0500

    fix gemspec file from version change
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/version.rb
+++ lib/rubycas-server-core/version.rb
@@ -1,11 +1,15 @@
-module RubyCAS::Server::Core
-  module Version # :nodoc: all
+module RubyCAS
+  module Server
+    module Core
+      module Version # :nodoc: all
 
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 0
-    TAG = 'alpha'
+        MAJOR = 0
+        MINOR = 1
+        PATCH = 0
+        TAG = 'alpha'
 
-    STRING = [MAJOR, MINOR, PATCH, TAG].join(',')
+        STRING = [MAJOR, MINOR, PATCH, TAG].join('.')
+      end
+    end
   end
 end
