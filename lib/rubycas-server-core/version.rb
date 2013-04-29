38266298756233385a60bc1616d5d6ffcfbbd7ce
Tyler Pickett <t.pickett66@gmail.com>
2013-04-29 14:25:58 -0500

    bring version module in line with typical ruby version layouts and capitalize CAS
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/version.rb
+++ lib/rubycas-server-core/version.rb
@@ -1,7 +1,15 @@
-module Rubycas
+module RubyCAS
   module Server
     module Core
-      VERSION = "0.0.1"
+      module Version # :nodoc: all
+
+        MAJOR = 0
+        MINOR = 1
+        PATCH = 0
+        TAG = 'alpha'
+
+        STRING = [MAJOR, MINOR, PATCH, TAG].join('.')
+      end
     end
   end
 end
