2fb7b01e0a679a6e8712f62f711d4b767a687d41
Tyler Pickett <t.pickett66@gmail.com>
2013-04-29 15:59:20 -0500

    add configuration method for Persistence module
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/persistence.rb
+++ lib/rubycas-server-core/persistence.rb
@@ -1,8 +1,19 @@
 module RubyCAS::Server::Core
   module Persistence
     class AdapterNotRegisteredError < StandardError; end
+    class AdapterNotSpecifiedError < StandardError; end
 
+    attr_reader :adapter
     @adapters ||= HashWithIndifferentAccess.new
+
+    def self.setup(config)
+      adapter_name = config.fetch(:adapter) {
+        raise AdapterNotSpecifiedError.new("No adapter specified in config file!")
+      }
+      adapter = adapter_named(adapter_name)
+      adapter.setup(config)
+      @adapter = adapter
+    end
 
     def self.register_adapter(adapter_name, handler)
       @adapters[adapter_name] = handler
--- spec/rubycas-server-core/persistence_spec.rb
+++ spec/rubycas-server-core/persistence_spec.rb
@@ -1,29 +1,41 @@
 require 'spec_helper'
 
 module RubyCAS::Server::Core
-  describe Persistence do
-    describe '.adapter_named' do
-      let(:name) { :awesome_adapter }
-      let(:klass) { Class.new }
+  describe Persistence, focus: true do
+    let(:name) { :awesome_adapter }
+    let(:klass) { double }
 
+    before do
+      Persistence.register_adapter(name, klass)
+    end
+
+    describe '.setup(config)' do
+      let(:config) { {
+        adapter: name.to_s,
+        address: '127.0.0.1',
+        password: 'super'
+      } }
+
+      before do
+        klass.should_receive(:setup).with(config)
+      end
+
+      it 'must pass the config on to the selected adapter' do
+        Persistence.setup(config)
+      end
+    end
+
+    describe '.adapter_named' do
       it 'must raise an error when an un-registered name is supplied' do
         expect{ Persistence.adapter_named(:garbage) }.to raise_error Persistence::AdapterNotRegisteredError
       end
 
       it 'must return the class registered to that name' do
-        Persistence.register_adapter(name, klass)
         Persistence.adapter_named(name.to_s).should eq klass
       end
     end
 
     describe '.register_adapter(adapter_name, handler)' do
-      let(:klass) { Class.new }
-      let(:name) { :super_awesome }
-
-      before do
-        Persistence.register_adapter(name, klass)
-      end
-
       it 'must allow us to look up the adapter class by giving the name' do
         Persistence.adapter_named(name).should eq klass
       end
