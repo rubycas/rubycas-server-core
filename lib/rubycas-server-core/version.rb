a70fc686bb7bd66b1303f09c55900d60818825b9
Tyler Pickett <t.pickett66@gmail.com>
2013-04-29 14:25:58 -0500

    bring version module in line with typical ruby version layouts
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/version.rb
+++ lib/rubycas-server-core/version.rb
@@ -1,7 +1,11 @@
-module Rubycas
-  module Server
-    module Core
-      VERSION = "0.0.1"
-    end
+module RubyCAS::Server::Core
+  module Version # :nodoc: all
+
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 0
+    TAG = 'alpha'
+
+    STRING = [MAJOR, MINOR, PATCH, TAG].join(',')
   end
 end
