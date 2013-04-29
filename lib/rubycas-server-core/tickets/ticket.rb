cd3ad324af245f9530ff16ce1450a359e0c34224
Tyler Pickett <t.pickett66@gmail.com>
2013-04-29 14:50:26 -0500

    add documentation to abstract Ticket class
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/tickets/ticket.rb
+++ lib/rubycas-server-core/tickets/ticket.rb
@@ -1,18 +1,39 @@
 require_relative '../util'
 
 module RubyCAS::Server::Core::Tickets
+
+  # Abstract class for shared ticket functionality
+  #
+  # @author Tyler Pickett
+  # @abstract
+  # @since 0.1.0.alpha
+  # @attr [Time] created_at When the ticket was first initialized
+  # @attr id The id assigned to this ticket by the persistence layer
+  # @attr [Time] last_used_at When the ticket was last used
+  # @attr [Fixnum] times_used The number of times the ticket has been used
   class Ticket
+
+    # Raised when an attribute is passed to the constructor that doesn't
+    # have a setter.
     class UnknownAttributeError < StandardError; end
+
+    # Raised on intialization when the inherited class doesn't specify
+    # how to prefix ticket strings
     class TicketPrefixNotSet < StandardError; end
+
+    # Raised when trying to validate a ticket but an expiration policy
+    # hasn't been set
     class ExpirationPolicyNotSet < StandardError; end
 
     class << self
-      # doing this as a class instance variable because class variables are evil
+      # Ivar for expiration policy class to delegate validation to.
       attr_accessor :expiration_policy
+
+      # Ivar for use in generating ticket strings, gets prepended to the
+      # randomly generated portion. Best set in the subclass' definition
       attr_reader :ticket_prefix
     end
 
-    # common attributes across all tickets
     attr_accessor :created_at, :id, :last_used_at, :ticket, :times_used
 
     def initialize(attributes = {})
@@ -22,14 +43,19 @@ module RubyCAS::Server::Core::Tickets
       @times_used ||= 0
     end
 
+    # retreives the expiration policy set by configs for the subclass
     def expiration_policy
       self.class.expiration_policy
     end
 
+    # Adds ease to storing tickets. Delegates actual work to the persistence
+    # module.
     def save
       RubyCAS::Server::Core::Persistence.save_ticket(self)
     end
 
+    # Enables tickets to be checked for validity, delegates actual work to the
+    # expiration policy set for the class.
     def valid?
       if expiration_policy.present?
         expiration_policy.ticket_valid?(self)
