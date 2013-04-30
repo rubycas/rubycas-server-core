5e6efb68cf1abe2f12727cd8567183268108dd6b
Tyler Pickett <t.pickett66@gmail.com>
2013-04-30 14:51:37 -0500

    implement super simple in memory ticket storage
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/persistence/in_memory.rb
+++ lib/rubycas-server-core/persistence/in_memory.rb
@@ -7,5 +7,15 @@ module RubyCAS::Server::Core::Persistence
       # InMemory adapter do not require any settings
       new
     end
+
+    def initialize
+      @tickets = {}
+    end
+
+    def save_ticket(ticket)
+      ticket.id ||= ticket.ticket
+      @tickets[ticket.class] ||= {}
+      @tickets[ticket.class][ticket.id] = ticket.attributes
+    end
   end
 end
--- spec/rubycas-server-core/persistence/in_memory_spec.rb
+++ spec/rubycas-server-core/persistence/in_memory_spec.rb
@@ -2,8 +2,28 @@ require 'spec_helper'
 
 module RubyCAS::Server::Core::Persistence
   describe InMemory do
+    let(:config) { {} }
+    let(:adapter) { RubyCAS::Server::Core::Persistence.adapter }
+    let(:ticket) { RubyCAS::Server::Core::Tickets::LoginTicket.new }
+
+    before(:all) do
+      RubyCAS::Server::Core::Persistence.setup({adapter: 'in_memory'})
+    end
+
     it 'must register it self with the persistence module' do
       RubyCAS::Server::Core::Persistence.adapter_named(described_class.adapter_name).should eq described_class
+    end
+
+    describe '#save_ticket' do
+      describe 'for a new ticket' do
+        before do
+          RubyCAS::Server::Core::Persistence.save_ticket(ticket)
+        end
+
+        it 'must set the assigned id on the ticket passed in' do
+          ticket.id.should_not be_nil
+        end
+      end
     end
     end
   end
