db45bcbc1c2a024b915ef0cf292a0e88501b5045
Tyler Pickett <t.pickett66@gmail.com>
2013-04-30 09:56:33 -0500

    add empty abstract adapter class
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

---
+++ lib/rubycas-server-core/persistence/adapter.rb
@@ -0,0 +1,7 @@
+module RubyCAS::Server::Core::Peristence
+  # Abstract adapter class for persistence, subclass me to implment the full
+  # required behavior.
+  class Adapter
+
+  end
+end
---
+++ spec/rubycas-server-core/persistence/adapter_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module RubyCAS::Server::Core::Peristence
+  describe Adapter do
+
+  end
+end
