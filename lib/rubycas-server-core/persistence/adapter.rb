df5caaa881225b81de82b79cf84f764aaef860e3
Tyler Pickett <t.pickett66@gmail.com>
2013-04-30 10:03:09 -0500

    add basic functionality for abstract persistence adapter
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/rubycas-server-core/persistence/adapter.rb
+++ lib/rubycas-server-core/persistence/adapter.rb
@@ -2,6 +2,17 @@ module RubyCAS::Server::Core::Peristence
   # Abstract adapter class for persistence, subclass me to implment the full
   # required behavior.
   class Adapter
+    class << self
+      attr_reader :adapter_name
+    end
 
+    def self.register_as(my_name)
+      RubyCAS::Server::Core::Persistence.register_adapter(my_name, self)
+      @adapter_name = my_name
+    end
+
+    def self.setup(config)
+      raise NotImplementedError.new("Setup is not implemented for the abstract case, please override me in your implementation.")
+    end
   end
 end
